fafd594ec7ebab643f31005947f0311dcdc4e404
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-08-09 11:40:20 +0530

    Base Constants

---
+++ src/Constants.ts
@@ -0,0 +1,3 @@
+export const USER_AGENT =
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
+export const API_BASE_URL = 'https://genius.com/api'
